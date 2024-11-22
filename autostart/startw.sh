af2556ee1e73b9d57b3fc688d59f723b7ea652cc
razvanh255 <54369479+razvanh255@users.noreply.github.com>
2024-11-22 17:46:38 +0200

    Add files via upload

---
+++ autostart/startw.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Press shift in 5 seconds"
+showkey -k > /tmp/keylog &
+sleep 5
+pkill showkey
+if grep -q "42" /tmp/keylog || grep -q "54" /tmp/keylog; then
+	echo "End script"
+	exit 0
+else echo "Start script"
+cd ..
+cd /home/razvanh/MagicMirror
+lxterminal -e npm run start:wayland &
+fi
---
+++ autostart/startw1b.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Press shift in 5 seconds"
+showkey -k > /tmp/keylog &
+sleep 5
+pkill showkey
+if grep -q "42" /tmp/keylog || grep -q "54" /tmp/keylog; then
+	echo "End script"
+	exit 0
+else echo "Start script"
+cd ..
+cd /home/razvanh/MagicMirror
+export MM_CONFIG_FILE=config/config1b.js
+lxterminal -e npm run start:wayland &
+fi
+
