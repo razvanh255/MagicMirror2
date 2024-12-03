ee31ab220b2ec16b0aebd6e8589d1149f20befd7
razvanh255 <54369479+razvanh255@users.noreply.github.com>
2024-12-03 16:08:01 +0200

    Update startw.sh

--- autostart/startw.sh
+++ autostart/startw.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-echo "Press shift in 5 seconds"
-showkey -k > /tmp/keylog &
-sleep 5
-pkill showkey
-if grep -q "42" /tmp/keylog || grep -q "54" /tmp/keylog; then
-	echo "End script"
-	exit 0
-else echo "Start script"
 cd ..
 cd /home/razvanh/MagicMirror
 export MM_CONFIG_FILE=config/config3.js
